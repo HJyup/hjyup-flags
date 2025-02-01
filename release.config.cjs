fc014d6140ca3b9e9071e03a2be9c5d1095da60e
danyilbutov <danyil.butov@solidgate.com>
2025-02-01 14:00:21 +0000

    fix: semantic release changes

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,7 @@
 module.exports = {
-  branches: ['main', { name: 'next', prerelease: true }],
+  branches: [
+    { name: 'main' },
+    { name: 'pre/rc', channel: 'pre/rc', prerelease: 'rc' },
+    { name: 'beta', channel: 'beta', prerelease: true },
+  ],
 };
